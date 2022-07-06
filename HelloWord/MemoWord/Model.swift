8889acc042daa82e0bb74e73eefc73fcb92b23ce
Burak Altunoluk <63430143+BurakAltunoluk@users.noreply.github.com>
2022-07-06 10:05:27 +0100

    First Update

---
+++ HelloWord/MemoWord/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  MemoWord
+//
+//  Created by Burak Altunoluk on 06/07/2022.
+//
+
+import Foundation
+
+struct mainModel {
+    var wordsArray: [String]
+    var meaningArray: [String]
+}
